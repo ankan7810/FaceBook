ca5afab4df867d93cfc72793d65c0d2830362f8d
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-06-11 21:06:05 +0530

    Update Mail.js to use Gmail service for transport

--- Backend/Config/Mail.js
+++ Backend/Config/Mail.js
@@ -9,9 +9,7 @@ dotenv.config(
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
