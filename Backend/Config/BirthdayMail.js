0a0224298bffc8f334870f17d51aa47584d98c7a
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-06-11 21:06:17 +0530

    Update email transport configuration to use Gmail service

--- Backend/Config/BirthdayMail.js
+++ Backend/Config/BirthdayMail.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
  dotenv.config({ path: "./.env" });
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
