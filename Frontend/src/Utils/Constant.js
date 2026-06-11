0d31ceead721f461b41853b149d5cfd06e933d13
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-06-11 14:43:35 +0530

    Update Constant.js

--- Frontend/src/Utils/Constant.js
+++ Frontend/src/Utils/Constant.js
@@ -1,3 +1,3 @@
-export const AUTH_BASE_URL = "http://localhost:3000/api/v1/auth";
-export const POST_BASE_URL = "http://localhost:3000/api/v1/post";
-export const BASE_URL = "http://localhost:3000/api/v1";
+export const AUTH_BASE_URL = "https://facebook-backend-6nqa.onrender.com/api/v1/auth";
+export const POST_BASE_URL = "https://facebook-backend-6nqa.onrender.com/api/v1/post";
+export const BASE_URL = "https://facebook-backend-6nqa.onrender.com/api/v1"
